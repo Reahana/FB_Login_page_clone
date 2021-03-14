4a76cb812611beb764075cabd321818e939a14e5
Amanda-G10 <reahana233@gmail.com>
2021-03-14 08:43:24 +0600

    add js

---
+++ loginPg.js
@@ -0,0 +1,13 @@
+function check(form)
+{
+
+ if(form.userid.value =="amanda@me.com" && form.pswrd.value =="123456")
+  {
+    window.open('https://reahana.github.io/My-Profiles-Clone/MyFacebook/Myhome.html')
+  }
+ else
+ {
+   alert("Error Password or Username")
+  }
+
+}
